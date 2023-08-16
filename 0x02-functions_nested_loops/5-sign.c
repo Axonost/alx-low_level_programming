0ad28165be29cc80a9244336ef6c7eb305719084
Axonost <elakrad.tarik@gmail.com>
2023-08-15 19:40:06 -0700

    function and nested loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,14 +7,18 @@
  * 0 and prints 0 if n is zero
  * -1 and prints - if n is less than zero
  */
-int print_sign(int n) {
+int print_sign(int n)  
+{
     if (n > 0) {
         _putchar(43);  /*  ASCII for '+' */
         return (1);
-    } else if (n == 0) {
+    
+    } 
+    else if (n == 0) {
         _putchar(48);  /*  ASCII for '0' */
         return (0);
-    } else {
+    } 
+    else {
         _putchar(45);  /*  ASCII for '-' */
         return (-1);
     }
